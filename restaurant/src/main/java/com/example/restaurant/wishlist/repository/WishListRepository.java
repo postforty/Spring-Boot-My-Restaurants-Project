d8ff685f8599d57ba0c4a84f329c59e0ffd9aa33
postforty <75767073+postforty@users.noreply.github.com>
2022-08-06 08:30:02 +0900

    [ADD] DB, TEST

---
+++ restaurant/src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -0,0 +1,11 @@
+package com.example.restaurant.wishlist.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.restaurant.db.MemoryDbRepositoryAbstract;
+import com.example.restaurant.wishlist.entity.WishListEntity;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+}
