92340defb079e4d9c672e801a5e6ad4a23a63e39
postforty <75767073+postforty@users.noreply.github.com>
2022-08-09 16:40:18 +0900

    [ADD] 네이버 검색 api

---
+++ restaurant/src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -0,0 +1,28 @@
+package com.example.restaurant.naver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NaverClient {
+
+	@Value("${naver.client.id}")
+	private String naverClientId;
+	
+	@Value("${naver.client.secret}")
+	private String naverSecret;
+	
+	@Value("${naver.url.search.local}")
+	private String naverLocalSearchUrl;
+	
+	@Value("${naver.url.search.image}")
+	private String naverImageSearchUrl;
+	
+	public void localSearch() {
+		
+	}
+	
+	public void imageSearch() {
+		
+	}
+}
