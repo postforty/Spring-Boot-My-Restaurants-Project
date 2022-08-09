3e21ab2d688167ae311d3048473795e130a63ad4
postforty <75767073+postforty@users.noreply.github.com>
2022-08-09 17:28:11 +0900

    [UPDATE] NaverClient

--- restaurant/src/main/java/com/example/restaurant/naver/NaverClient.java
+++ restaurant/src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -1,7 +1,17 @@
 package com.example.restaurant.naver;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.naver.dto.SearchLocalRes;
 
 @Component
 public class NaverClient {
@@ -10,7 +20,7 @@ public class NaverClient {
 	private String naverClientId;
 	
 	@Value("${naver.client.secret}")
-	private String naverSecret;
+	private String naverClientSecret;
 	
 	@Value("${naver.url.search.local}")
 	private String naverLocalSearchUrl;
@@ -18,8 +28,29 @@ public class NaverClient {
 	@Value("${naver.url.search.image}")
 	private String naverImageSearchUrl;
 	
-	public void localSearch() {
+	public SearchLocalRes localSearch(SearchLocalReq searchLocalReq) {
+		var uri = UriComponentsBuilder.fromUriString(naverLocalSearchUrl)
+				.queryParams(searchLocalReq.toMultiValueMap())
+				.build()
+				.encode()
+				.toUri();
 		
+		var headers = new HttpHeaders();
+		headers.set("X-Naver-Client-Id", naverClientId);
+		headers.set("X-Naver-Client-Secret", naverClientSecret);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		var httpEntity = new HttpEntity<>(headers);
+		var responseType = new ParameterizedTypeReference<SearchLocalRes>() {
+		};
+		
+		var responseEntity = new RestTemplate().exchange(
+				uri,
+				HttpMethod.GET,
+				httpEntity,
+				responseType
+		);
+		return responseEntity.getBody();
 	}
 	
 	public void imageSearch() {
